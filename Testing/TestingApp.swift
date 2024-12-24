b939e270470464dd6d941427fd0f1b1804db712a
Phal-Madina <phalmadina11@gmail.com>
2024-12-24 11:26:33 +0700

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Phal Madina on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestingTests/TestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestingTests.swift
+//  TestingTests
+//
+//  Created by Phal Madina on 24/12/24.
+//
+
+import Testing
+@testable import Testing
+
+struct TestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
